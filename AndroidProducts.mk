cb434ad7a584c581fadc3f3ff00b2157dc788ac4
root <root@localhost.localdomain>
2024-10-04 00:34:42 +0600

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_Symphony_Z50.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_Symphony_Z50-user \
+    twrp_Symphony_Z50-userdebug \
+    twrp_Symphony_Z50-eng
