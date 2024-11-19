e5b9507a6b81760c1ff74839db4683e7e63e2559
zobaermd620 <zobaermd620@gmail.com>
2024-11-19 18:00:39 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,58 @@
-add_lunch_combo twrp_Symphony_Z50-user 
-add_lunch_combo twrp_Symphony_Z50-userdebug 
-add_lunch_combo twrp_Symphony_Z50-eng
+FDEVICE="Symphony_Z50"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then 
+
+
+
+
+
+
+
+# Version & Variant
+ export FOX_VERSION="R12.1"
+ export FOX_VARIANT="A14"
+ export FOX_BUILD_TYPE="Stable"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
