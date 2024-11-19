b61888b397f6139f3707431cdaadd03d64049f0c
zobaermd620 <zobaermd620@gmail.com>
2024-11-19 18:03:53 +0600

    Create fox_Symphony_Z50.mk

---
+++ fox_Symphony_Z50.mk
@@ -0,0 +1,38 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2023 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+
+# Custom pic for maintainers about section info
+OF_MAINTAINER := "HASIB"
+
+# Screen settings
+OF_SCREEN_H := "2160"
+OF_STATUS_H := "80"
+OF_STATUS_INDENT_LEFT := "40"
+OF_STATUS_INDENT_RIGHT := "40"
+
+
+##export OF_HIDE_NOTCH := 1
+##export OF_CLOCK_POS := 1
+OF_ALLOW_DISABLE_NAVBAR := 0
+
+# Maximum permissible splash image size (in kilobytes); do *NOT* increase
+OF_SPLASH_MAX_SIZE := "4096"
+OF_ADVANCED_SECURITY := 0
