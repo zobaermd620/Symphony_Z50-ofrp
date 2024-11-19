0bfdb22d4867358b66bd70855e4f19ea56396b82
mdhaaib200520 <mdhasib200520@gmail.com>
2024-11-19 10:26:21 +0600

    twrp_Symphony_Z50.mk

--- twrp_Symphony_Z50.mk
+++ twrp_Symphony_Z50.mk
@@ -16,25 +16,25 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+
+
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Inherit Telephony packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 
 # Inherit 64bit support
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Symphony_Z50
-PRODUCT_NAME := omni_Symphony_Z50
+PRODUCT_NAME := twrp_Symphony_Z50
 PRODUCT_BRAND := symphony
 PRODUCT_MODEL := symphony Symphony_Z50
 PRODUCT_MANUFACTURER := symphony
